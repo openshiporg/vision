5ee5c1e88c97b0999ad7add7578631aab08d64aa
Junaid <junaidkabani3@gmail.com>
2022-11-05 12:15:05 -0500

    Update layout

--- pages/index.js
+++ pages/index.js
@@ -50,64 +50,6 @@ function Content() {
   );
 }
 
-function Overlay() {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        pointerEvents: "none",
-        width: "100%",
-        height: "100%",
-      }}
-    >
-      <a
-        href="https://pmnd.rs/"
-        style={{ position: "absolute", bottom: 40, left: 90, fontSize: "13px" }}
-      >
-        pmnd.rs
-        <br />
-        dev collective
-      </a>
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate3d(-50%,-50%,0)",
-        }}
-      >
-        <h1
-          style={{
-            margin: 0,
-            padding: 0,
-            fontSize: "15em",
-            fontWeight: 500,
-            letterSpacing: "-0.05em",
-          }}
-        >
-          hello
-        </h1>
-      </div>
-      <div
-        style={{ position: "absolute", top: 40, left: 40, fontSize: "13px" }}
-      >
-        pretty bad —
-      </div>
-      <div
-        style={{
-          position: "absolute",
-          bottom: 40,
-          right: 40,
-          fontSize: "13px",
-        }}
-      >
-        25/02/2022
-      </div>
-    </div>
-  );
-}
 
 function Stars(props) {
   const ref = useRef();
@@ -291,7 +233,7 @@ export default function IndexPage() {
           <Text font="/Ki-Medium.ttf" fontSize={0.5} position={[0, -3.4, 0]}>
             We're standardizing the marketplace and then building{" "}
           </Text>
-          <mesh position={[0.2, -3.3, -3]}>
+          <mesh position={[0.2, -4.35, 0]}>
             <mesh
               position={[-1.1, -0.08, 0]}
               scale="3.2"
